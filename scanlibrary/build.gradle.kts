89a222dc3e68e40144419952d75c39bedb57c8d5
Ahmed Mones <agprosup@gmail.com>
2024-11-25 01:31:38 +0300

    update library version to 1.0.5
    (cherry picked from commit cd0299667896b61dd26be02a30da7ed7cd2c649b)

--- scanlibrary/build.gradle.kts
+++ scanlibrary/build.gradle.kts
@@ -88,7 +88,7 @@ publishing {
                 from(components["release"])
                 groupId = "com.github.Al-Taie"
                 artifactId = "document_scanner_lib_android"
-                version = "1.0.4"
+                version = "1.0.5"
             }
         }
     }
