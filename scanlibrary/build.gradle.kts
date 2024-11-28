de6c9e44b1836478040c4542725e473b4159b172
Ahmed Mones <agprosup@gmail.com>
2024-11-28 17:05:39 +0300

    update version to 1.0.6
    (cherry picked from commit 92e5bca8edd7379554a183d18d2af2345ee272a0)

--- scanlibrary/build.gradle.kts
+++ scanlibrary/build.gradle.kts
@@ -88,7 +88,7 @@ publishing {
                 from(components["release"])
                 groupId = "com.github.Al-Taie"
                 artifactId = "document_scanner_lib_android"
-                version = "1.0.5"
+                version = "1.0.6"
             }
         }
     }
